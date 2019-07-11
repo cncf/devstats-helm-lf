35b063eb2224743bbb052f1ba7609667d7008bc4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-11 09:52:12 +0000

    Added delete volume script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util/delete_volume.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#AWS_PROFILE=lfproduct-dev aws ec2 detach-volume --volume-id vol-05d73f1686cbfe351 --force --instance-id i-0b0e283b3b6021caf
+if [ -z "$AWS_PROFILE" ]
+then
+  echo "$0: you must set AWS_PROFILE=..."
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you must specify volume-id, like vol-05d73f1686cbfe351"
+  exit 2
+fi
+if [ -z "$2" ]
+then
+  aws ec2 detach-volume --volume-id "$1" --force
+else
+  aws ec2 detach-volume --volume-id "$1" --force --instance-id "$2"
+fi
+aws ec2 delete-volume --volume-id "$1"
